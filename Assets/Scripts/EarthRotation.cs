fa38c0de9a0ebc3ecb7e133ae89bbdd3a5ae80b3
unknown <unknown@unknown.com>
2016-08-17 10:52:20 +1200

    Sphere rotates now

---
+++ Assets/Scripts/EarthRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthRotation : MonoBehaviour
+{
+    public GameObject sphere;
+    public float degreesToRotate;
+
+    Transform thisTransform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        thisTransform = sphere.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    thisTransform.Rotate(degreesToRotate * Vector3.right * Time.deltaTime, Space.World);
+    }
+}

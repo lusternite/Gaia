8f313159780e746e6b75b7e5498005a278d6fcbf
Morg <morgarathftw@gmail.com>
2016-08-17 14:40:16 +1200

    f

---
+++ Assets/Scripts/GaiaManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GaiaManager : MonoBehaviour {
+
+    public int[] Africa;
+    public int[] Asia;
+    public int[] Australia;
+    public int[] Europe;
+    public int[] NorthAmerica;
+    public int[] SouthAmerica;
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

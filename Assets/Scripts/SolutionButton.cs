31bd4f72157ea91dcc5e5c99effd3755074645c5
Simon <luster_nite@hotmail.com>
2016-08-17 16:57:32 +1200

    Built UI stuff
    Have Scrolling events, popups, and solution buttons

---
+++ Assets/Scripts/SolutionButton.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SolutionButton : MonoBehaviour {
+
+    public string SolutionType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ChooseSolution()
+    {
+        switch (SolutionType)
+        {
+            case "EARTHQUAKE":
+                {
+
+                    break;
+                }
+            case "VOLCANO":
+                {
+
+                    break;
+                }
+            case "TSUNAMI":
+                {
+
+                    break;
+                }
+            case "FLOOD":
+                {
+
+                    break;
+                }
+            case "HURRICANE":
+                {
+
+                    break;
+                }
+            case "TORNADO":
+                {
+
+                    break;
+                }
+            case "DROUGHT":
+                {
+
+                    break;
+                }
+            case "FAMINE":
+                {
+
+                    break;
+                }
+            case "HEATWAVE":
+                {
+
+                    break;
+                }
+            case "BLIZZARD": //....entertainment
+                {
+
+                    break;
+                }
+            case "WILDFIRE":
+                {
+
+                    break;
+                }
+        }
+    }
+}
